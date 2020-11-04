775c66723ac869edcca4bae86c5b9eb3c9526834
shivamj2603 <shivamj2603@gmail.com>
2020-11-05 03:29:54 +0530

    Added: Test case to check whether the contact data is updated and in sync with the database

--- src/test/java/addressbooktest/AddressBookTest.java
+++ src/test/java/addressbooktest/AddressBookTest.java
@@ -1,5 +1,7 @@
 package addressbooktest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.SQLException;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import com.addressbook.AddressBookService;
@@ -12,6 +14,15 @@ public class AddressBookTest {
 		AddressBookService addressBookService = new AddressBookService();
 		List<Contact> contactData = addressBookService.readContactDBData();
 		assertEquals(4, contactData.size());
+	}
+	@Test
+	public void givenNewDataForContact_WhenUpdated_ShouldBeInSync() throws DatabaseException, SQLException {
+		AddressBookService addressBookService = new AddressBookService();
+		List<Contact> contactData = addressBookService.readContactDBData();
+		addressBookService.updateContactData("Shivam", "Jaiswal", Long.parseLong("7748454018"));
+		addressBookService.readContactDBData();
+		boolean result = addressBookService.checkContactDataSync("Shivam", "Jaiswal");
+		assertEquals(true, result);
 	}
 
 }
