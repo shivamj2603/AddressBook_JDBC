f2e1c74b7d85b7738b79617e95a0bc5a8359becb
shivamj2603 <shivamj2603@gmail.com>
2020-11-06 20:19:10 +0530

    Added: Test cases to check for the insertion of contacts in the database

--- src/test/java/com/capgemini/addressbooktest/AddressBookServiceTest.java
+++ src/test/java/com/capgemini/addressbooktest/AddressBookServiceTest.java
@@ -1,13 +1,17 @@
-package addressbooktest;
+package com.capgemini.addressbooktest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.sql.SQLException;
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import com.addressbook.AddressBookService;
-import com.addressbook.Contact;
-import com.addressbook.DatabaseException;
+
+import com.capgemini.addressbook.AddressBookService;
+import com.capgemini.addressbook.Contact;
+import com.capgemini.addressbook.DatabaseException;
 
 public class AddressBookServiceTest {
 	@Test
@@ -46,8 +50,21 @@ public class AddressBookServiceTest {
 	@Test
 	public void givenContact_WhenAdded_ShouldMatchCount() throws DatabaseException, SQLException {
 		AddressBookService addressBookService = new AddressBookService();
-		addressBookService.addContact("Ramesh", "Powar", "Malad", "Kerala", 421201, 1, "20324843854", "rameshpowar@gmail.com");
+		addressBookService.addContact("Ramesh", "Powar", "Malad", "Kerala", 421201, "20324843854", "rameshpowar@gmail.com", 2);
 		List<Contact> contactList = addressBookService.readContactDBData();
 		assertEquals(contactList.size(), 5);
+	}
+	@Test
+	public void givenMultipleContacts_WhenAddedToDB_ShouldMatchContactEntries() throws DatabaseException {
+		Contact[] contactArray = { new Contact("Locky","Feguson", "Malad","Maharashtra",412055, 2324235324L,"abcd@gmail.com",2),
+				new Contact("Locky","Feguson", "Malad","Maharashtra",412055, 2324235324L,"abcd@gmail.com",1)};
+		AddressBookService addressBookService = new AddressBookService();
+		addressBookService.readContactDBData();
+		Instant start = Instant.now();
+		addressBookService.addContactToDB(Arrays.asList(contactArray));
+		Instant threadEnd = Instant.now();
+		System.out.println("Duration with Thread: " + Duration.between(start, threadEnd));
+		long result = addressBookService.readContactDBData().size();
+		assertEquals(7, result);
 	}
 }
